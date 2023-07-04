25dd68e4b8891698528e9e21809551ee5aa5af36
williamtanuwijaya <will.tw0807@gmail.com>
2023-07-04 09:21:32 +0700

    update

--- public/js/form.js
+++ public/js/form.js
@@ -12,74 +12,60 @@ let arrayInputPassword = [];
 
 // Jika belum mengisi semua kolom di register
 const form_register = document.getElementById('register').addEventListener('submit', function (event) {
+  event.preventDefault();
+
   let inputName = document.getElementById('name').value;
   let inputNoHp = document.getElementById('nohp').value;
   let inputEmail = document.getElementById('email_signup').value;
   let inputPassword = document.getElementById('psw_signup').value;
 
-  if (inputName == '') {
-    alert('Name required.');
+  if (inputName === '' || inputNoHp === '' || inputEmail === '' || inputPassword === '') {
+    alert('Please fill in all fields.');
     kontainer.classList.add('register-mode');
     return;
-  } else if (inputNoHp == '') {
-    alert('No Hp required.');
-    kontainer.classList.add('register-mode');
-    return;
-  } else if (inputEmail == '') {
-    alert('Email required.');
-    kontainer.classList.add('register-mode');
-    return;
-  } else if (inputPassword == '') {
-    alert('password required.');
-    kontainer.classList.add('register-mode');
-    return;
-  } else if (arrayInputEmail.indexOf(inputEmail) == '-1') {
-    arrayInputEmail.push(inputEmail);
-    arrayInputName.push(inputName);
-    arrayInputNoHp.push(inputNoHp);
-    arrayInputPassword.push(inputPassword);
-
-    document.getElementById('name').value = '';
-    document.getElementById('nohp').value = '';
-    document.getElementById('email_signup').value = '';
-    document.getElementById('psw_signup').value = '';
-
-    alert(email + '  Thanks for registration. \nTry to login Now');
-    kontainer.classList.remove('register-mode');
-  } else {
-    alert(inputEmail + ' is already register.');
+  } else if (arrayInputEmail.includes(inputEmail)) {
+    alert(inputEmail + ' is already registered.');
     kontainer.classList.add('register-mode');
     return;
   }
+
+  arrayInputEmail.push(inputEmail);
+  arrayInputName.push(inputName);
+  arrayInputNoHp.push(inputNoHp);
+  arrayInputPassword.push(inputPassword);
+
+  document.getElementById('name').value = '';
+  document.getElementById('nohp').value = '';
+  document.getElementById('email_signup').value = '';
+  document.getElementById('psw_signup').value = '';
+
+  alert(inputEmail + ' Thanks for registration. \nTry to login Now');
+  kontainer.classList.remove('register-mode');
 });
 
 const form_login = document.getElementById('login').addEventListener('submit', function (event) {
+  event.preventDefault();
+
   let inputEmail = document.getElementById('email_signin').value;
   let inputPassword = document.getElementById('psw_signin').value;
   let i = arrayInputEmail.indexOf(inputEmail);
 
-  if (arrayInputEmail.indexOf(inputEmail) == -1) {
-    if (inputEmail == '') {
-      alert('Email required.');
-      return;
-    } else {
-      alert('Email tidak ada.');
+  if (arrayInputEmail.includes(inputEmail)) {
+    if (arrayInputPassword[i] !== inputPassword) {
+      alert('Incorrect password.');
+      window.location.href = '/';
       return;
     }
-  } else if (arrayInputPassword[i] != inputPassword) {
-    if (inputEmail == '') {
-      alert('password required.');
-      return;
-    }
-    alert('password tidak sesuai.');
-    return;
+
+    alert(inputEmail + ' you are logged in. \nWelcome to our website.');
+    window.location.href = '/dashboard';
   } else {
-    alert(inputEmail + ' you are login Now \n welcome to our website.');
-    window.open('/dasboard');
+    alert('Email not found.');
+    window.location.href = '/';
   }
 
-  document.getElementById('email_signup').value = '';
-  document.getElementById('psw_signup').value = '';
+  document.getElementById('email_signin').value = '';
+  document.getElementById('psw_signin').value = '';
 });
 
 register_btn.addEventListener('click', () => {
