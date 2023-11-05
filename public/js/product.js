5c91ada30c836a3d025209be73a67c4c7b7f334d
williamtanuwijaya <will.tw0807@gmail.com>
2023-11-05 18:48:05 +0700

    deploy to vercel

--- public/js/product.js
+++ public/js/product.js
@@ -1,42 +1,42 @@
 // PRIMARY KEY ID PRODUK
 let idProduk = '00';
 // VARIABEL UNTUK Menampilkan Card Confirm
-  let confirm_SourceImg ;
-  let confirm_namaProduk;
-  let confirm_hargaProduk;
+let confirm_SourceImg;
+let confirm_namaProduk;
+let confirm_hargaProduk;
 
 // Tambah Produk
 document.addEventListener('DOMContentLoaded', function () {
-  tambahProduk('./assets/Jordan Series ES.jpg', 'Jordan Series ES', '1.399.000');
-  tambahProduk("./assets/Nike Blazer Low '77 Jumbo.jpg", "Nike Blazer Low '77 Jumbo", '2.199.000');
-  tambahProduk('./assets/Nike Pegasus 40.jpg', 'Nike Pegasus 40', '2.399.000');
-  tambahProduk('./assets/Nike Tanjun Racer Lifestyle Shoes.jpg', 'Nike Tanjun Racer', '1.990.000');
-  tambahProduk('./assets/Jumpman MVP.jpg', 'Jumpman MVP', '1.099.000');
-  tambahProduk('./assets/Jumpman Two Trey.jpg', 'Jumpma Two Grey', '1.799.000');
-  tambahProduk('./assets/Nike Air Max 90.jpg', 'Nike Air Max 90', '1.799.000');
-  tambahProduk("./assets/Zion 2 'Full Moon' PF.jpg", "Zion 2 'Full Moon", '1.799.000');
-  tambahProduk("./assets/Nike Air Force 1 '07.jpg", "Nike Air Force '07'", '1.299.000');
-  tambahProduk('./assets/Nike GT Jump.png', 'Nike GT Jump', '1.799.000');
-  tambahProduk('./assets/Nike React Phantom Run Flyknit 2.jpg', 'Phantom Run Flyknit 2', '1.799.000');
-  tambahProduk('./assets/KD Trey 5 X EP.jpg', 'KD Trey 5 X EP ', '1.999.000');
-  tambahProduk('./assets/Jordan Stadium 90.jpg', 'Jordan Stadium 90', '1.499.000');
-  tambahProduk('./assets/Jordan Stay Loyal 2.png', 'Jordan Stay Loyal', '1.799.000');
-  tambahProduk('./assets/Nike ACG Watercat+.png', 'Nike ACG Watercat', '2.299.000');
-  tambahProduk('./assets/Nike Air Flight Lite Mid.png', 'Nike Air Flight Lite Mid', '2.199.000');
-  tambahProduk('./assets/Nike Cosmic Unity 2.png', 'Nike Cosmic Unity 2', '2.099.000');
-  tambahProduk('./assets/Nike Dunk High Retro Premium.png', 'Nike Dunk High Retro Premium', '1.699.000');
-  tambahProduk('./assets/Nike Elevate 3.png', 'Nike Elevate 3', '1.999.000');
-  tambahProduk('./assets/Nike Go FlyEase.png', 'Nike Go FlyEase', '1.999.000');
-  tambahProduk('./assets/Nike Killshot 2 Leather.png', 'Nike KillShot 2 Leather', '2.009.000');
-  tambahProduk('./assets/Nike Tech Hera.png', 'Nike Tech Hera', '1.699.000');
-  tambahProduk('./assets/Nike Tempo.png', 'Nike Tempo', '2.199.000');
-  tambahProduk('./assets/Nike Vomero 16.png', 'Nike Vomero 16', '1.599.000');
-  tambahProduk('./assets/Nike SuperRep Go.jpg', 'Nike SuperPrep Go', '1.099.000');
-  tambahProduk('./assets/Nike Impact 4.jpg', 'Nike Impact 4', '1.349.000');
-  tambahProduk('./assets/Air Jordan 1 Mid.jpg', 'Air Jordan 1 Mid', '1.939.000');
-  tambahProduk('./assets/Nike Air VaporMax 2023 Flyknit.jpg', 'Nike Air VaporMax 2023 FlyNit', '3.269.000');
-  tambahProduk('./assets/Nike Air Max Plus 3.jpg', 'Nike Air Max Plus 3', '2.989.000');
-  tambahProduk('./assets/Nike Air Presto.jpg', 'Nike Air Presto', '2.199.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Jordan Series ES.jpg', 'Jordan Series ES', '1.399.000');
+  tambahProduk("http://127.0.0.1:5500/public/assets/Nike Blazer Low '77 Jumbo.jpg", "Nike Blazer Low '77 Jumbo", '2.199.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Pegasus 40.jpg', 'Nike Pegasus 40', '2.399.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Tanjun Racer Lifestyle Shoes.jpg', 'Nike Tanjun Racer', '1.990.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Jumpman MVP.jpg', 'Jumpman MVP', '1.099.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Jumpman Two Trey.jpg', 'Jumpma Two Grey', '1.799.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Air Max 90.jpg', 'Nike Air Max 90', '1.799.000');
+  tambahProduk("http://127.0.0.1:5500/public/assets/Zion 2 'Full Moon' PF.jpg", "Zion 2 'Full Moon", '1.799.000');
+  tambahProduk("http://127.0.0.1:5500/public/assets/Nike Air Force 1 '07.jpg", "Nike Air Force '07'", '1.299.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike GT Jump.png', 'Nike GT Jump', '1.799.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike React Phantom Run Flyknit 2.jpg', 'Phantom Run Flyknit 2', '1.799.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/KD Trey 5 X EP.jpg', 'KD Trey 5 X EP ', '1.999.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Jordan Stadium 90.jpg', 'Jordan Stadium 90', '1.499.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Jordan Stay Loyal 2.png', 'Jordan Stay Loyal', '1.799.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike ACG Watercat+.png', 'Nike ACG Watercat', '2.299.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Air Flight Lite Mid.png', 'Nike Air Flight Lite Mid', '2.199.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Cosmic Unity 2.png', 'Nike Cosmic Unity 2', '2.099.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Dunk High Retro Premium.png', 'Nike Dunk High Retro Premium', '1.699.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Elevate 3.png', 'Nike Elevate 3', '1.999.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Go FlyEase.png', 'Nike Go FlyEase', '1.999.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Killshot 2 Leather.png', 'Nike KillShot 2 Leather', '2.009.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Tech Hera.png', 'Nike Tech Hera', '1.699.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Tempo.png', 'Nike Tempo', '2.199.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Vomero 16.png', 'Nike Vomero 16', '1.599.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike SuperRep Go.jpg', 'Nike SuperPrep Go', '1.099.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Impact 4.jpg', 'Nike Impact 4', '1.349.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Air Jordan 1 Mid.jpg', 'Air Jordan 1 Mid', '1.939.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Air VaporMax 2023 Flyknit.jpg', 'Nike Air VaporMax 2023 FlyNit', '3.269.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Air Max Plus 3.jpg', 'Nike Air Max Plus 3', '2.989.000');
+  tambahProduk('http://127.0.0.1:5500/public/assets/Nike Air Presto.jpg', 'Nike Air Presto', '2.199.000');
 });
 
 let cartItems = [];
@@ -51,6 +51,7 @@ function tambahProduk(sourceImg, namaProduk, hargaProduk) {
   let divCardOverlayContent = document.createElement('div');
   let p = document.createElement('p');
   let buttonAddToCart = document.createElement('button');
+  console.log('Image Source:', sourceImg);
 
   idProduk++; // Increment idProduk
 
@@ -74,9 +75,9 @@ function tambahProduk(sourceImg, namaProduk, hargaProduk) {
   imgCardImage.setAttribute('src', sourceImg);
   imgCardImage.setAttribute('alt', namaProduk);
 
-  buttonAddToCart.setAttribute('type','button');
-  buttonAddToCart.setAttribute('data-bs-toggle','modal');
-  buttonAddToCart.setAttribute('data-bs-target','#staticBackdrop');
+  buttonAddToCart.setAttribute('type', 'button');
+  buttonAddToCart.setAttribute('data-bs-toggle', 'modal');
+  buttonAddToCart.setAttribute('data-bs-target', '#staticBackdrop');
 
   divCardOverlayContent.appendChild(p);
   divCardOverlayContent.appendChild(buttonAddToCart);
@@ -89,14 +90,16 @@ function tambahProduk(sourceImg, namaProduk, hargaProduk) {
 
   let container = document.getElementById('containers');
   container.appendChild(divCard);
-// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   buttonAddToCart.addEventListener('click', function () {
-
     let idCard = this.getAttribute('data-id');
     let namaProduk = document.getElementById('produk-' + idCard).querySelector('.card-title').textContent;
     let hargaProduk = document.getElementById('produk-' + idCard).querySelector('.card-price').textContent;
-    let sourceImg = document.getElementById('produk-' +idCard).querySelector('.card-image').getAttribute('src');
+    let sourceImg = document
+      .getElementById('produk-' + idCard)
+      .querySelector('.card-image')
+      .getAttribute('src');
 
     hargaProduk = hargaProduk.replace('Harga: Rp ', '').replace(/\D/g, '');
     hargaProduk = parseFloat(hargaProduk);
@@ -110,8 +113,6 @@ function tambahProduk(sourceImg, namaProduk, hargaProduk) {
       existingItem.jumlah++;
       existingItem.hargaTotal = existingItem.jumlah * hargaProduk;
       document.getElementById('item-' + existingItem.id).textContent = `${existingItem.namaProduk} - ${existingItem.jumlah} barang - Rp ${existingItem.hargaTotal.toLocaleString('id-ID')}.00`;
-
-
     } else {
       let idDetailTransaksi = cartItems.length + 1;
       let newCartItem = {
@@ -126,8 +127,6 @@ function tambahProduk(sourceImg, namaProduk, hargaProduk) {
       detailTransaksi.textContent = `${newCartItem.namaProduk} - ${newCartItem.jumlah} barang - Rp ${newCartItem.hargaTotal.toLocaleString('id-ID')}.00`;
       transactionItems.appendChild(detailTransaksi);
     }
-
-    
 
     // Menampung jumlah barang
     let totalBarang = 0;
